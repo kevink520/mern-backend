f6357171acf504ed078807d1c029231f3cf2876d
Kevin K <kevinkim75@gmail.com>
2019-12-15 12:34:37 -0500

    Changed password minlength

--- routes/users-routes.js
+++ routes/users-routes.js
@@ -8,12 +8,12 @@ router.get('/', getAllUsers);
 router.post('/signup', [
   check('name').not().isEmpty(),
   check('email').normalizeEmail().isEmail(),
-  check('password').isLength({ min: 7 }),
+  check('password').isLength({ min: 6 }),
 ], signUpUser);
 
 router.post('/login', [
   check('email').normalizeEmail().isEmail(),
-  check('password').isLength({ min: 5 }),
+  check('password').isLength({ min: 6 }),
 ], loginUser);
 
 module.exports = router;
